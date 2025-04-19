15ee7924475d11ce7aa8ff2d429022aaf41351db
Abbas Syed <syed.abbas2593@gmail.com>
2025-04-19 10:06:23 +0300

    updated backedn ur

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7109'
+  // apiBaseUrl: 'https://localhost:7109'
+  apiBaseUrl: 'https://karesapi.azurewebsites.net'
+
 };

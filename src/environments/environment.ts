95fe2791e8ab8f555fb40a1444dbfedf90f11002
Abbas <abbassyed.dev@gmail.com>
2025-05-19 17:01:51 +0300

    add charts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:64072'
-  // apiBaseUrl: 'https://karesapi.azurewebsites.net'
+  // apiBaseUrl: 'https://localhost:64072'
+  apiBaseUrl: 'https://karesapi.azurewebsites.net'
 
 };

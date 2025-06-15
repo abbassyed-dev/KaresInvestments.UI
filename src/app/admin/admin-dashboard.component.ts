dac5b1bb2631c99a7d560ea74507bd67f4b312c9
Abbas <abbassyed.dev@gmail.com>
2025-06-15 17:20:03 +0300

    currency format in stmt + route back to previous tab

--- src/app/admin/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../layout/service/app.layout.service';
 import { User } from '../models/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './admin-dashboard.component.html',
@@ -10,10 +11,16 @@ export class AdminDashboardComponent implements OnInit {
   activeTabIndex = 0;
   userContext: User = {} as User;
 
-  constructor(public layoutService: LayoutService) {}
+  constructor(public layoutService: LayoutService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     console.log('Admin Dashboard');
+    this.route.queryParams.subscribe(params => {
+      const tab = +params['tab'];
+      if (!isNaN(tab)) {
+        this.activeTabIndex = tab;
+      }
+    });
   }
 
   updateUserContext(user: User) {
@@ -25,7 +32,13 @@ export class AdminDashboardComponent implements OnInit {
   }
 
   handleChange(e: any) {
+    const tabIndex = e.index;
     console.log('Index is :', e.index);
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { tab: tabIndex },
+      queryParamsHandling: 'merge',
+    });
     this.userContext = {} as User;
   }
 }
